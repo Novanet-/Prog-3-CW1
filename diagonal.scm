47405fc90fe4ef201c48e7068c8c674f721ac293
Novanet- <gamemaster8888@gmail.com>
2015-10-27 21:10:28 +0000

    Got matrix diagonal working

--- diagonal.scm
+++ diagonal.scm
@@ -1,10 +1,21 @@
 (define diagonal
-  )
+  (lambda (m)
+    (if (matrix? m)
+      (calcDiagonal m 0)
+       return)))
 
 (define matrix?
   (lambda (m)
-    (list-equal? (map length m))))
+    (if (list-equal? (map length m))
+    '()
+    (error "Not a Rectangular Matrix"))))
 
+(define calcDiagonal
+  (lambda (m n)
+    (if (null? m)
+        '()
+        (cons (list-ref (car m) n) (calcDiagonal (cdr m) (+ n 1))))))
+     
 (define list-equal?
   (lambda (l)
     (apply = l)))
