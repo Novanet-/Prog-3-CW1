14f35cd459055509538b1978d257494c100f52c8
Will <gamemaster8888@gmail.com>
2015-10-31 23:33:19 +0000

    Commented q1 and q2

--- diagonal.scm
+++ diagonal.scm
@@ -1,15 +1,22 @@
+;;Checks if the given argument is a square matrix, if not, then retunr from the program, otherwise
+;;calculate the diagonal of a matrix
 (define diagonal
   (lambda (m)
     (if (matrix? m)
       (calcDiagonal m 0)
        '())))
 
+;;Determines if an argument is a matrix (square), by checkign that the length of all rows
+;;is equal
 (define matrix?
   (lambda (m)
     (if (list-equal? (map length m))
     '()
     (error "Not a Rectangular Matrix"))))
 
+;;Calculates the diagonal of a matrix recursively, by taking the car of the first row, the
+;;cdr of the second row, incrementing by 1 of the list-ref of the row element accessed
+;;and then repeating this proccess until the end of the matrix has been reached
 (define calcDiagonal
   (lambda (m n)
     (if
@@ -17,6 +24,7 @@
         '()
         (cons (list-ref (car m) n) (calcDiagonal (cdr m) (+ n 1))))))
      
+;;Determines if all elements of a list are equal
 (define list-equal?
   (lambda (l)
     (apply = l)))
