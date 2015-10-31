84d34e55c145c633c3f8cc4d375b2e3ad404f068
Will <gamemaster8888@gmail.com>
2015-10-31 18:46:33 +0000

    Finished q2

--- increasing-average.scm
+++ increasing-average.scm
@@ -1,15 +1,27 @@
+#lang scheme
+
 (define increasing-average
-  (lambda (list)
-    (if (checkAverage list 0 (car list))
-        #t
-        #f)))
+  (lambda (lst)
+    (if (null? lst)
+     (error "No average")
+     (if (checkAverage lst 0 (car lst))
+        (newline) 
+        #f))))
         
 (define checkAverage
-  (lambda (list n prevAvg)
-    (if (calculate_average list n) < prevAVG)
+  (lambda (lst n prevAvg)
+    (if (< (calculate-average lst n) prevAvg)
     #f
-    (checkAverage list (+ n 1))))
+    (if (= n (- (length lst) 1))
+     (display (calculate-average lst n))
+     (checkAverage lst (+ n 1) (calculate-average lst n))))))
         
 (define calculate-average
-  (lambda (list n)
-   (/ (apply + l) (length list))))
+  (lambda (lst n)
+   (/ (sum-numbers lst n 0) (+ n 1))))
+
+(define sum-numbers
+  (lambda (lst n acc)
+    (if (> acc n)
+        0
+        (+ (list-ref lst acc) (sum-numbers lst n (+ acc 1))))))
