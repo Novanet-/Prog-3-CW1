4c5d3e2c4c55e0bb668d8ea96fca4c8c35944208
Will <gamemaster8888@gmail.com>
2015-11-01 12:51:55 +0000

    Completed q3 and commented it

--- listset-union.scm
+++ listset-union.scm
@@ -1,44 +1,6 @@
-#lang scheme
-
-(begin
-  (define listset-add
-    (lambda (element set)
-      (if (member element set)
-          set
-          (cons element set))))
-
-
-  (define listset-empty '())
-  (define listset-null?
-    (lambda (set)
-      (equal? set listset-empty)))
-
-  (define listset-first car)
-  (define listset-rest cdr)
-
-  (define listset-included?
-    (lambda (set1 set2)
-      (if (listset-null? set1)
-          #t
-          (if (member (listset-first set1) set2)
-              (listset-included? (listset-rest set1) set2)
-              #f))))
-
-  (define listset-equal?
-    (lambda (set1 set2)
-      (and (listset-included? set1 set2)
-           (listset-included? set2 set1)))))
-
-(begin
-  (define set1 (listset-add '1 listset-empty))
-  (define set2 (listset-add '2 set1))
-  (define set3 (listset-add '3 set2))
-  
-  (define seta (listset-add 'a listset-empty))
-  (define setb (listset-add 'b seta))
-  (define setc (listset-add 'c setb))
-  (define setd (listset-add 'd setc)))
-
+;;Checks if either set is empty, if so, then return the other set
+;;If sets are equal, then jsut return the first set
+;;Otherwise, compute the union of the sets
 (define listset-union
   (lambda (set1 set2)
     (cond ((listset-null? set1) set2)
@@ -47,12 +9,14 @@
           (else
                (compute-union set1 set2)))))
 
+;;Base case: If second set is null, return first set
+;;If the current element from the second set is already in the first set, then move onto the next element without adding the
+;;element to the union;
+;;Otherwise, add the current element of the second set, to the first set, and compute the union with the first set
+;;(and all unioned elements form the second set) with the rest of the second set
 (define compute-union
   (lambda (set1 set2)
     (cond ((listset-null? set2) set1)
           ((member (listset-first set2) set1) (compute-union set1 (listset-rest set2)))
           (else (compute-union (listset-add (listset-first set2) set1) (listset-rest set2))))))
-
-;;(load "listset-union.scm")
-;;(listset-union set3 setd)
 
