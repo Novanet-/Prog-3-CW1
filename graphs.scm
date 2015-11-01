81b47a5357121970ae8701d7598f3f63cee6a9ca
Novanet- <gamemaster8888@gmail.com>
2015-11-01 21:51:20 +0000

    Tests logically pass, not in correct format for tester

--- graphs.scm
+++ graphs.scm
@@ -5,7 +5,7 @@
 (define graph-mapper
   (lambda (graph m)
     (cond ((null? graph) '())
-          ((null? m) graph)
+          ((null? m) '())
           (else (calculate-graph graph m)))))
 
 ;;If there are no more edges left to applying the map to, then return the empty list
@@ -13,9 +13,9 @@
 ;;functions in the map applied to it
 (define calculate-graph
   (lambda (graph m)
-    (if (null? (cdr graph))
+    (if (null? graph)
         '()
-        (cons (apply-map (car graph) m) (calculate-graph (cdr graph) m)))))
+        (cons (apply-map (car graph) m) (calculate-graph (cdr graph) m) ))))
 
 ;;If the given edge of the graph, or the map is empty, then return the empty list
 ;;Otherwise, apply the map functions in the map to the edge, replacing nodes according to the map
@@ -23,25 +23,23 @@
   (lambda (edge m)
     (cond ((or (null? edge) (null? m)) '())
          ;; (else (replace-nodes edge (car m))))))
-          (cons (replace-node sourcenode m) (replace-node destnode m)))))
-
-;;For each node in an edge, if it's the source node in a map function, then replace it with the destination node in that function
-;;(define replace-nodes
-;;  (lambda (edge map)
-;;    (let ((mapf (car map)))
-;;    (if (eqv? (sourcenode mapf) (sourcenode edge))
-;;        (if (eqv? (sourcenode mapf) (destnode edge))
-;;            (cons (destnode mapf) (destnode mapf))
-;;            (cons (destnode mapf) (destnode edge)))
-;;        (if (eqv? (sourcenode mapf) (destnode edge))
-;;            (cons (sourcenode edge) (destnode mapf))
-;;            (cons (sourcenode edge) (destnode edge)))))))
+          (else (if (and (node-in-map? (sourcenode edge) m)
+                         (node-in-map? (destnode edge) m))                 
+                    (cons (replace-node (sourcenode edge) m) (replace-node (destnode edge) m))
+                    '()
+                    )))))
 
 (define replace-node
-  (lambda node m)
-  (cond ((null? m) node)
-         (eqv? node (caar m) (cdar m))
-         (else (replace-node (cdr m)))))
+  (lambda (node m)
+  (cond ((null? m) '())
+        ((eqv? node (sourcenode (car m))) (destnode (car m)))
+         (else (replace-node node (cdr m))))))
+
+(define node-in-map?
+  (lambda (node m)
+    (cond ((null? m) #f)
+          ((eqv? node (sourcenode (car m))) #t)
+          (else (node-in-map? node (cdr m))))))
 
 (define sourcenode
   (lambda (edge)
@@ -49,8 +47,23 @@
 
 (define destnode
   (lambda (edge)
-    (cdr edge)))
-        
+    (if (list? (cdr edge))
+        (car (cdr edge))
+        (cdr edge))))
+
 ;;(load "graphs.scm")
+
+;;Passes first two tests, fails last three
+;;> (graph-mapper *graph1* *map1*)
+;;((a . b) (b . b) (b . a))
+;;> (graph-mapper *graph1* *map2*)
+;;((a1 . b1) (b1 . c1) (c1 . a1))
+;;> (graph-mapper *graph1* *map3*)
+;;((a . b) (b . c) (c . a))
+;;> (graph-mapper *graph1* *map4*)
+;;((a . b) (b . c) (c . a))
+;;> (graph-mapper *graph1* *map5*)
+;;((a b) (b c) (c a))
+    
     
         
