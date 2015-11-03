7dbdeb4677f6134f834b52a8ddbef307c24e60ed
Novanet <gamemaster8888@gmail.com>
2015-11-03 16:21:41 +0000

    Fixed test errors

--- diagonal.scm
+++ diagonal.scm
@@ -2,17 +2,9 @@
 ;;calculate the diagonal of a matrix
 (define diagonal
   (lambda (m)
-    (if (matrix? m)
       (calcDiagonal m 0)
-       '())))
-
-;;Determines if an argument is a matrix (square), by checkign that the length of all rows
-;;is equal
-(define matrix?
-  (lambda (m)
-    (if (list-equal? (map length m))
-    '()
-    (error "Not a Rectangular Matrix"))))
+  )
+)
 
 ;;Calculates the diagonal of a matrix recursively, by taking the car of the first row, the
 ;;cdr of the second row, incrementing by 1 of the list-ref of the row element accessed
@@ -22,13 +14,7 @@
     (if
      (or (null? m) (null? (car m)) (<= (length (car m)) n)) 
         '()
-        (cons (list-ref (car m) n) (calcDiagonal (cdr m) (+ n 1))))))
-     
-;;Determines if all elements of a list are equal
-(define list-equal?
-  (lambda (l)
-    (apply = l)))
-
-
-
-
+        (cons (list-ref (car m) n) (calcDiagonal (cdr m) (+ n 1)))
+    )
+   )
+)
